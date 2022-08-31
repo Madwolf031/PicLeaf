22db29c9c12a1fac3917ca95c17815b3987e2a0c
Madwolf <fritzlamparasofficial@gmail.com>
2022-08-31 23:37:23 +0800

    test

--- app/src/main/java/com/example/picleaf/Categorization.kt
+++ app/src/main/java/com/example/picleaf/Categorization.kt
@@ -1,20 +1,26 @@
 package com.example.picleaf
 
 import android.content.res.AssetManager
+import android.graphics.Bitmap
 import org.tensorflow.lite.Interpreter
 import java.io.FileInputStream
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 import java.nio.MappedByteBuffer
 import java.nio.channels.FileChannel
+import java.util.*
+import kotlin.collections.ArrayList
 
-class Categorization (assetManager: AssetManager, modelPath: String, labelPath: String,inputSize: Int) {
+class Categorization(assetManager: AssetManager,modelPath: String, labelPath: String, inputSize: Int){
     private val GVN_INP_SZ: Int = inputSize
     private val PHOTO_SDEVIATE = 255.0f
-    private val GREAT_OUTCOM_MXX = 3
+    private val GREAT_OUTCOME_MXX = 3
     private val PITNR: Interpreter
-    private var ROW_LINE = whereistopped
+    private var ROW_LINE: List<String>
     private val IMAGE_PXL_SZ: Int = 3
-    private val  PHOTO_MEN = 0
+    private val PHOTO_MEN = 0
     private val POINT_THRHLDD = 0.4f
+
 
     data class Categorization(
         var id: String = "",
@@ -27,7 +33,13 @@ class Categorization (assetManager: AssetManager, modelPath: String, labelPath: 
     }
     init {
         PITNR = Interpreter(loadModelFile(assetManager,modelPath))
-        ROW_LINE = loadlabellist(assetManager,labelPath)
+        ROW_LINE = loadlabellist (assetManager, labelPath)
+    }
+
+    private fun loadlabellist(assetManager: AssetManager, labelPath: String): List<String> {
+
+        return assetManager.open(labelPath).bufferedReader().useLines { it.toList() }
+
     }
 
     private fun loadModelFile(assetManager: AssetManager, modelPath: String): MappedByteBuffer {
@@ -35,9 +47,65 @@ class Categorization (assetManager: AssetManager, modelPath: String, labelPath: 
         val fileDescriptor = assetManager.openFd(modelPath)
         val inputStream = FileInputStream(fileDescriptor.fileDescriptor)
         val fileChannel = inputStream.channel
-        val starOffset = fileDescriptor.startOffset
+        val startOffset = fileDescriptor.startOffset
         val declaredLength = fileDescriptor.declaredLength
-        return fileChannel.map(FileChannel.MapMode)
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY,startOffset,declaredLength)
 
     }
+
+    fun recognizeImage (bitmap: Bitmap): List<com.example.picleaf.Categorization.Categorization>{
+
+        val scaledBitmap = Bitmap.createScaledBitmap(bitmap, GVN_INP_SZ, GVN_INP_SZ, false)
+        val byteBuffer = convertBitmapToByteBuffer(scaledBitmap)
+        val result = Array(1){ FloatArray(ROW_LINE.size)}
+        PITNR.run(byteBuffer,result)
+        return getSortedResult(result)
+
+    }
+
+    private fun getSortedResult(result: Array<FloatArray>): List<com.example.picleaf.Categorization.Categorization> {
+        val pq = PriorityQueue(
+            GREAT_OUTCOME_MXX,
+            kotlin.Comparator<com.example.picleaf.Categorization.Categorization> {
+                    (_, _, confidence1), (_, _, confidence2)
+                     ->  java.lang.Float.compare(confidence1,confidence2)* -1 }
+        )
+        for (i in ROW_LINE.indices){
+            val confidence = result[0][i]
+            if (confidence >= POINT_THRHLDD){
+                pq.add(com.example.picleaf.Categorization.Categorization(
+                    "" +i,
+                    if (ROW_LINE.size>i) ROW_LINE[i] else "Unknown", confidence
+                ))
+            }
+        }
+        val recognitions = ArrayList<com.example.picleaf.Categorization.Categorization>()
+        val recognitionsSize = Math.min(pq.size,GREAT_OUTCOME_MXX)
+        for (i in 0 until recognitionsSize){
+            recognitions.add(pq.poll())
+        }
+        return recognitions
+    }
+
+    private fun convertBitmapToByteBuffer(scaledBitmap: Bitmap?): ByteBuffer {
+        val byteBuffer = ByteBuffer.allocateDirect(4*GVN_INP_SZ*GVN_INP_SZ*IMAGE_PXL_SZ)
+        byteBuffer.order(ByteOrder.nativeOrder())
+        val intValue = IntArray (GVN_INP_SZ*GVN_INP_SZ)
+
+        scaledBitmap!!.getPixels(intValue, 0,scaledBitmap.width,0,0,scaledBitmap.width,scaledBitmap.height)
+        var pixel = 0
+        for (i in 0 until GVN_INP_SZ){
+            for (j in 0 until GVN_INP_SZ) {
+                val `val` = intValue[pixel++]
+
+                byteBuffer.putFloat((((`val`.shr(16) and 0xFF) - PHOTO_MEN)/PHOTO_SDEVIATE))
+                byteBuffer.putFloat((((`val`.shr(8) and 0xFF) - PHOTO_MEN)/PHOTO_SDEVIATE))
+                byteBuffer.putFloat((((`val` and 0xFF) - PHOTO_MEN)/PHOTO_SDEVIATE))
+
+            }
+        }
+        return  byteBuffer
+
+    }
+
 }
--- app/src/main/java/com/example/picleaf/MainActivity.kt
+++ app/src/main/java/com/example/picleaf/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.picleaf
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mCategorization: Categorization
+    private lateinit var mBitmap: Bitmap
+    private val mCameraRequestCode = 0
+    private val mGalleryRequestCode = 2
+
+    private val mInputSize = 224
+    private val mModelPath = "plant_disease_model.tflite"
+    private val mLabelPath = "plant_labels.txt"
+
+    private val mSamplePath = "automn.jpg"
+    lateinit var toolbar: Toolbar
+    // lateinit var drawer: AdvanceDrawerLayout //
+    lateinit var navigationView: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
